e4d8c49edea6c8070c4b33593c52c1f44919997d
El-EnderJ <130647064+El-EnderJ@users.noreply.github.com>
2023-05-08 08:51:37 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    radio.sendString("when the impostor is sus")
+    radio.sendString("1")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    radio.sendString("4")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    radio.sendString("3")
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    if (receivedString == 1) {
+        basic.showLeds(`
+            . # # # .
+            # . . # #
+            # . . # #
+            . # # # #
+            . # . # .
+            `)
+    } else if (receivedString == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    } else if (receivedString == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("2")
 })
 radio.setGroup(56)
